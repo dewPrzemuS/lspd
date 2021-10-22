6349d502cbd917fc3e51b9dffcbff58527788d09
Przemuś! <przemuslive@gmail.com>
2021-10-22 14:42:36 +0200

    init

---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+
+from flask import Flask
+
+from routes.error import error_bp
+from routes.home import home_bp
+
+app = Flask(__name__)
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"  # Leave it like this!
+
+app.register_blueprint(home_bp)
+
+if __name__ == "__main__":
+    # sys.setdefaultencoding('utf8mb4_polish_ci')
+    app.config['JSON_AS_ASCII'] = False
+    app.config['RESTFUL_JSON'] = {
+        'ensure_ascii': False
+    }
+    app.run(port=80, debug=False)
---
+++ routes/home.py
@@ -0,0 +1,48 @@
+from flask import Blueprint, redirect, render_template, request
+import json
+
+jsonFile = open("data.json")
+
+data = json.loads(jsonFile.read())
+
+home_bp = Blueprint('Home', __name__)
+
+
+
+def getalnum(string):
+    alphanumeric = [character for character in string if character.isalnum()]
+    return "".join(alphanumeric)
+
+
+@home_bp.route("/", methods=["GET"])
+def homeget():
+    return render_template("index.html", data=data, getalnum=getalnum, len=len)
+
+
+@home_bp.route("/", methods=["POST"])
+def homepost():
+    fine = 0
+    pp = 0
+    jail = 0
+    global division
+    global display
+    display = True
+    if request.form.get("range") == "0":
+        division = 3
+    if request.form.get("range") == "1":
+        division = 2
+    if request.form.get("range") == "2":
+        division = 1
+    for d in data:
+        value = request.form.get(f"option{getalnum(d)}")
+        if value is None:
+            continue
+        fine += (data[value]["fine"] / division)
+        pp += (data[value]["pp"] / division)
+        jail += (data[value]["jail"] / division)
+        fine = round(fine)
+        pp = round(pp)
+        jail = round(jail)
+    if fine == 0 and pp == 0 and jail == 0:
+        display = False
+    return render_template("index.html", data=data, getalnum=getalnum, len=len, fine=fine, pp=pp, jail=jail, display=display)
