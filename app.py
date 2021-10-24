8679253c38e1c85f4532fae64e9013b2ab8b1b03
Przemuś! <przemuslive@gmail.com>
2021-10-24 12:28:21 +0200

    removed ssh stuff 1.1.4

--- app.py
+++ app.py
@@ -11,9 +11,4 @@ os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"  # Leave it like this!
 app.register_blueprint(home_bp)
 
 if __name__ == "__main__":
-    # sys.setdefaultencoding('utf8mb4_polish_ci')
-    app.config['JSON_AS_ASCII'] = False
-    app.config['RESTFUL_JSON'] = {
-        'ensure_ascii': False
-    }
     app.run(debug=False)
--- functions/database.py
+++ functions/database.py
@@ -2,52 +2,40 @@ import json
 import os
 
 import mysql.connector
-import sshtunnel
-
-sshtunnel.SSH_TIMEOUT = 15.0
-sshtunnel.TUNNEL_TIMEOUT = 15.0
 
 jsonFileMySQL = open(f"{os.path.dirname(os.path.realpath(__file__))}/special/mysql.json")
 sql = json.loads(jsonFileMySQL.read())
 
-jsonFileSSH = open(f"{os.path.dirname(os.path.realpath(__file__))}/special/ssh.json")
-ssh = json.loads(jsonFileSSH.read())
+db = mysql.connector.connect(
+    host=sql["host"],
+    user="uefh3c1k7ynedfai",
+    password=sql["password"],
+    database="b2wphrwtexyixckyex1x"
+)
 
-with sshtunnel.SSHTunnelForwarder(
-        ("ssh.eu.pythonanywhere.com", 22),
-        ssh_username=ssh["username"],
-        ssh_password=ssh["password"],
-        remote_bind_address=(sql["host"], 3306)
-) as server:
-    db = mysql.connector.connect(
-        host=sql["host"],
-        user="uefh3c1k7ynedfai",
-        password=sql["password"],
-        database="b2wphrwtexyixckyex1x"
-    )
-
-    cursor = db.cursor()
+cursor = db.cursor()
 
 
-    def addEnter(ip, date, method):
-        sql = "INSERT INTO entries (ip, date, method) VALUES (%s, %s, %s)"
-        val = (ip, date, method)
-        cursor.execute(sql, val)
-        db.commit()
+def addEnter(ip, date, method):
+    sql = "INSERT INTO entries (ip, date, method) VALUES (%s, %s, %s)"
+    val = (ip, date, method)
+    cursor.execute(sql, val)
+    db.commit()
 
 
-    def addPenalty(ip, date, range, selected):
-        sql = "INSERT INTO penalties (ip, date, penaltyRange, selected) VALUES (%s, %s, %s, %s)"
-        val = (ip, date, range, selected)
-        cursor.execute(sql, val)
-        db.commit()
+def addPenalty(ip, date, range, selected):
+    sql = "INSERT INTO penalties (ip, date, penaltyRange, selected) VALUES (%s, %s, %s, %s)"
+    val = (ip, date, range, selected)
+    cursor.execute(sql, val)
+    db.commit()
 
 
-    def getPenalties():
-        sql = "SELECT penaltyRange FROM penalties"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        return result
+def getPenalties():
+    sql = "SELECT penaltyRange FROM penalties"
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    return result
+
 
 jsonFileData = open(f"{os.path.dirname(os.path.realpath(__file__))}/data.json")
 data = json.loads(jsonFileData.read())
