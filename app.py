865b2af185a517bdc57005427fd4be5da1c66085
Przemuś! <przemuslive@gmail.com>
2021-10-24 23:27:45 +0200

    fix for vps nginx 1.1.4

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 import os
 
+# from OpenSSL import SSL
 from flask import Flask
 
 from routes.home import home_bp
 
 app = Flask(__name__)
 
+# context = SSL.Context(SSL.SSLv23_METHOD)
+# cer = os.path.join(os.path.dirname(__file__), 'resources/udara.com.crt')
+# key = os.path.join(os.path.dirname(__file__), 'resources/udara.com.key')
+
+
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"  # Leave it like this!
 
 app.register_blueprint(home_bp)
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=80, ssl_context="adhoc")
+    app.run(debug=False, host="127.0.0.1", port=5000)
