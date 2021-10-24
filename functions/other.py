6000fc3c8e4942d538db42933a1cd2aaabdbcede
Przemuś! <przemuslive@gmail.com>
2021-10-24 23:53:43 +0200

    another getip update

--- functions/other.py
+++ functions/other.py
@@ -3,6 +3,6 @@ from flask import request
 
 def getIP():
     if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
-        return request.environ['REMOTE_ADDR']
+        return str(request.environ['REMOTE_ADDR'])
     else:
-        return ['HTTP_X_FORWARDED_FOR']
+        return str(request.environ['HTTP_X_FORWARDED_FOR'])
