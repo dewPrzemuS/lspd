5dc8b542adea02d9afbab3c995c638f55b2cbbf8
klamka123123 <46725495+dewprzemus@users.noreply.github.com>
2022-03-16 13:20:07 +0100

    1.1.5 debug fix

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ app.register_blueprint(home_bp)
 app.register_blueprint(error_bp)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=False, host="0.0.0.0", port=5000)
