af17ac7a544b98b6fb3eb760d3ab194ffb424e50
devPrzemuS <46725495+dewPrzemuS@users.noreply.github.com>
2022-06-25 12:08:38 +0200

    1.2
    bardziej mobile friendly jest teraz i wyjebałem jakąś nieużywaną od 50 lat funkcję getip idk po co była xd

--- functions/other.py
+++ functions/other.py
@@ -5,10 +5,3 @@ from flask import request
 
 jsonFileData = open(f"{os.path.dirname(os.path.realpath(__file__))}/data.json")
 data = json.loads(jsonFileData.read())
-
-
-def getIP():
-    if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
-        return str(request.environ['REMOTE_ADDR'])
-    else:
-        return str(request.environ['HTTP_X_FORWARDED_FOR'])
