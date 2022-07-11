6f611c04e1cb5c4ceef851b64c34850706836fe2
devPrzemuS <46725495+dewPrzemuS@users.noreply.github.com>
2022-07-11 16:33:23 +0200

    XDDD

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from flask import Flask
 
 from routes.index import index_bp
 from routes.lspd import lspd_bp
-from routes.support import support_bp
+# from routes.support import support_bp
 
 app = Flask(__name__)
 
 app.register_blueprint(lspd_bp)
-app.register_blueprint(support_bp)
+# app.register_blueprint(support_bp)
 app.register_blueprint(index_bp)
 
 if __name__ == "__main__":
