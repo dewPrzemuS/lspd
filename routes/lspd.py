cb1bbcd510c418aeb6302ebd25cbdaf93e84042c
devPrzemuS <46725495+dewPrzemuS@users.noreply.github.com>
2022-08-04 17:49:26 +0200

    1.6

--- routes/lspd.py
+++ routes/lspd.py
@@ -20,6 +20,8 @@ def lspdGet():
                            amount2=amount2, display=False)
 
 
+# create class/object of Explanation that contains the explanation and type of explanation
+
 @lspd_bp.route("/lspd", methods=["POST"])
 def lspdPost():
     global amount0
@@ -39,8 +41,12 @@ def lspdPost():
     global selected
     selected = []
     display = True
-    global explanations
-    explanations = ""
+
+    global fineExplanations
+    fineExplanations = ""
+
+    global jailExplanations
+    jailExplanations = ""
     if request.form.get("range") == "0":
         penaltyType = "Minimalny"
     if request.form.get("range") == "1":
@@ -56,13 +62,26 @@ def lspdPost():
         value = request.form.get(f"option{getalnum(d)}")
         if value is None:
             continue
-        if dataLspd[value]["explanation"] != "null":
-            explanations += dataLspd[value]["explanation"] + ", "
+
         selected.append(d)
         pp += dataLspd[value]["pp"]
         if request.form.get("range") == "0":
-            fine += dataLspd[value]["minfine"]
-            jail += dataLspd[value]["minjail"]
+            if dataLspd[value]["jailorfine"]:
+                if request.form.get("jailorfine") == "on":
+                    jail += dataLspd[value]["minjail"]
+                    if jail < 1 and dataLspd[value]["jail"] > 0:
+                        jail += 1
+                else:
+                    fine += dataLspd[value]["minfine"]
+                    if fine < 1 and dataLspd[value]["fine"] > 0:
+                        fine += 1
+            else:
+                jail += dataLspd[value]["minjail"]
+                if jail < 1 and dataLspd[value]["jail"] > 0:
+                    jail += 1
+                fine += dataLspd[value]["minfine"]
+                if fine < 1 and dataLspd[value]["fine"] > 0:
+                    fine += 1
         else:
             if dataLspd[value]["fine"] / division < dataLspd[value]["minfine"]:
                 fine += dataLspd[value]["minfine"]
@@ -77,16 +96,41 @@ def lspdPost():
                     jail -= (dataLspd[value]["jail"] / division)
                 else:
                     fine -= (dataLspd[value]["fine"] / division)
+
+        # if explanation is not empty
+        if dataLspd[value]["explanation"] != "":
+            # if data is jailorfine
+            if dataLspd[value]["jailorfine"]:
+                # if jailorfine is on
+                if request.form.get("jailorfine") == "on":
+                    # if jail is greater than 0
+                    if dataLspd[value]["jail"] > 0:
+                        # add explanation to list
+                        jailExplanations += dataLspd[value]["explanation"] + ", "
+                else:
+                    # if fine is greater than 0
+                    if dataLspd[value]["fine"] > 0:
+                        # add explanation to list
+                        fineExplanations += dataLspd[value]["explanation"] + ", "
+            else:
+                if dataLspd[value]["jail"] > 0:
+                    # add explanation to list
+                    jailExplanations += dataLspd[value]["explanation"] + ", "
+                if dataLspd[value]["fine"] > 0 or dataLspd[value]["pp"] > 0:
+                    # add explanation to list
+                    fineExplanations += dataLspd[value]["explanation"] + ", "
     fine = round(fine)
     pp = round(pp)
     jail = round(jail)
+
     if jail > 250:
         jail = 250
     if fine > 5000:
         fine = 5000
 
-    explanations = explanations[:-2] + "."
+    jailExplanations = jailExplanations[:-2] + "."
+    fineExplanations = fineExplanations[:-2] + "."
 
     return render_template("lspd.html", data=dataLspd, getalnum=getalnum, len=len, fine=fine, pp=pp, jail=jail,
                            display=display, round=round, amount0=amount0, amount1=amount1,
-                           amount2=amount2, selected=selected, penaltyType=penaltyType, explanations=explanations)
+                           amount2=amount2, selected=selected, penaltyType=penaltyType, fineExplanations=fineExplanations, jailExplanations=jailExplanations)
